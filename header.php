9a8b4b0ffcbf4cd96d0bc534af8cab2ba3efc365
Matt Steele <matt@matt-steele.com>
2017-04-23 12:20:58 -0600

    Update head meta.

--- header.php
+++ header.php
@@ -6,17 +6,32 @@
 
 <head>
 
+    <title><?php echo $site_name; ?></title>
+
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?php echo $site_name; ?></title>
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
+    <meta name="description" content="***UPDATE***" />
+    <meta name="keywords" content="***UPDATE***" />
+    <meta name="author" content="***UPDATE***" />
 
- <!--   ADD FAVICONS -->
+    <!-- OPEN GRAPH -->
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content="***UPDATE***">
+    <meta property="og:url" content="***UPDATE***">
+    <meta property="og:image" content="/dist/img/opengraph.png">
+
+    <!-- FAVICONS -->
+    <link rel="apple-touch-icon" sizes="152x152" href="/dist/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/dist/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/dist/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/dist/favicons/manifest.json">
+    <link rel="mask-icon" href="/dist/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/dist/favicons/favicon.ico">
+    <meta name="msapplication-config" content="/dist/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 
 <link rel="stylesheet" href="<?php echo $site_url; ?>dist/css/style.min.css">
 
